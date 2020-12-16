f2ae37d4b156e81276a80702098559b38d19f0ca
wass <wassim.filali@gmail.com>
2020-12-16 12:18:42 +0100

    sync interrupt running

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,28 @@
 #include <Arduino.h>
 
-#define led_on()  digitalWrite(PC13, LOW)
-#define led_off()  digitalWrite(PC13, HIGH)
-#define debug_on()  digitalWrite(PB13, HIGH)
-#define debug_off()  digitalWrite(PB13, LOW)
+#define LED_PIO   PC13
+#define DEBUG_PIO PB13
+#define RELAY_PIO PB4
+//sync test PB6
+//sync prod PB5
+#define SYNC_PIO  PB6
+
+#define led_on()  digitalWrite(LED_PIO, LOW)
+#define led_off()  digitalWrite(LED_PIO, HIGH)
+
+#define debug_on()  digitalWrite(DEBUG_PIO, HIGH)
+#define debug_off()  digitalWrite(DEBUG_PIO, LOW)
+
+#define relay_on()    digitalWrite(RELAY_PIO, HIGH)
+#define relay_off()   digitalWrite(RELAY_PIO, LOW)
 
 HardwareSerial Serial3(PB11, PB10);
+
+void sync_irq(){
+  debug_on();
+  delayMicroseconds(5);
+  debug_off();
+}
 
 void start_timer_freq(TIM_TypeDef *Instance,uint32_t freq,callback_function_t callback){
   HardwareTimer *MyTim = new HardwareTimer(Instance);
@@ -16,14 +33,23 @@ void start_timer_freq(TIM_TypeDef *Instance,uint32_t freq,callback_function_t ca
 
 void setup()
 {
-  pinMode(PC13, OUTPUT);
-  pinMode(PB13, OUTPUT);
+  pinMode(LED_PIO, OUTPUT);       //led
+  pinMode(DEBUG_PIO, OUTPUT);     //debug
+  pinMode(RELAY_PIO, OUTPUT);     //relay
+  pinMode(SYNC_PIO, INPUT_PULLUP);//sync
+
   led_on();
   delay(300);
   led_off();
 
+  debug_on();
+  delayMicroseconds(100);
+  debug_off();
+
+
   Serial3.begin(115200);
   //start_timer_freq(TIM1,1000,cdc_consumer);
+  attachInterrupt(digitalPinToInterrupt(SYNC_PIO),sync_irq,RISING);
 
 }
 
